38b920cdb0d75356e9337b8888117142af7cd306
stavroskiig <stavroskiig@gmail.com>
2023-07-31 19:28:32 +0300

    Created Recipe Model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  recipes = [];
 
+  constructor() { }
+
+  ngOnInit() {}
 }
